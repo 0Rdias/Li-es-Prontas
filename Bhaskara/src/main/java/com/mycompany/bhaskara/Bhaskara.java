e742d14c50034153b0e70165179c2b5214f95d06
0Rdias <raul.ds.ds64nes@gmail.com>
2025-04-03 13:53:24 -0300

    Add files via upload

---
+++ Bhaskara/src/main/java/com/mycompany/bhaskara/Bhaskara.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bhaskara;
+
+/**
+ *
+ * @author dti
+ */
+public class Bhaskara {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
